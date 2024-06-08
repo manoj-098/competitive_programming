b0ef03b006585f8d733812d8612dc5d108dce20b
Manoj V <manojvenki098@gmail.com>
2024-06-08 18:51:43 +0530

    Create Armstrong.java
    https://www.geeksforgeeks.org/problems/armstrong-numbers2727/1

---
+++ DSA/Basic_Math/Armstrong.java
@@ -0,0 +1,8 @@
+if (b == 0)
+            return a;
+ 
+        // call the gcd() method recursively by
+        // replacing a with b and b with
+        // modulus(a,b) as long as b != 0
+        else
+            return gcd(b, a % b);
