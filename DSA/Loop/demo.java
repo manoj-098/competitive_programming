873f790c23bfec9892555ceda9477eeacd4baae8
Manoj V <manojvenki098@gmail.com>
2024-06-08 19:33:08 +0530

    Create demo.java

---
+++ DSA/Loop/demo.java
@@ -0,0 +1,13 @@
+    static boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+ 
+        // Check from 2 to sqrt(n)
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0)
+                return false;
+ 
+        return true;
+    }
