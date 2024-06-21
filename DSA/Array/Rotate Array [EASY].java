4330196fdf6c9bdc6a8b87a7e0ebc43e35dca6f4
Manoj V <manojvenki098@gmail.com>
2024-06-21 09:33:19 +0530

    Create Rotate Array [EASY].java
    https://www.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1

---
+++ DSA/Array/Rotate Array [EASY].java
@@ -0,0 +1,26 @@
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d, int n) {
+        // add your code here
+        d%=n;
+        reverse(arr,d,n-1);//reverse the remainnig part
+        reverse(arr,0,d-1);//reverse the rotatable part
+        reverse(arr,0,n-1);//reverse entire array
+        //refer this for explanation: https://www.geeksforgeeks.org/program-for-array-rotation-continued-reversal-algorithm/
+    }
+    
+    static void reverse(int[] arr,int start,int end)
+    {
+        while(start<end)
+        {
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+//this code takes O(N) time complexity and O(1) extra space i.e: in-place
