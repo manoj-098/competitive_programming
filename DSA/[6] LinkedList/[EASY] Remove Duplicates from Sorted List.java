f6b45bbae6a81c4fc039d9bc296074785cd175f6
Manoj V <manojvenki098@gmail.com>
2024-07-02 18:17:37 +0530

    Update [EASY] Remove Duplicates from Sorted List.java
    https://leetcode.com/problems/remove-duplicates-from-sorted-list/

--- DSA/[6] LinkedList/[EASY] Remove Duplicates from Sorted List.java
+++ DSA/[6] LinkedList/[EASY] Remove Duplicates from Sorted List.java
@@ -10,21 +10,33 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null || h ead.next==null) return head;
-        ListNode start=head;
-        ListNode end=head.next;
-        while(start.next!=null)
+        if(head==null || head.next==null) return head;
+
+        //APPROACH 1: Using Two pointer
+        // ListNode start=head;
+        // ListNode end=head.next;
+        // while(start.next!=null)
+        // {
+        //     if(end.val==start.val)
+        //     {
+        //         start.next=end.next;
+        //         end=start.next;
+        //     }
+        //     else
+        //     {
+        //         start=start.next;
+        //         end=end.next;
+        //     }
+        // }
+
+        //APPROACH 2:Adjacent Comparison (without two pointer)
+        ListNode temp=head;
+        while(temp.next!=null)
         {
-            if(end.val==start.val)
-            {
-                start.next=end.next;
-                end=start.next;
-            }
-            else
-            {
-                start=start.next;
-                end=end.next;
-            }
+            if(temp.val==temp.next.val)
+                temp.next=temp.next.next;
+
+            else temp=temp.next;
         }
         return head;
     }
