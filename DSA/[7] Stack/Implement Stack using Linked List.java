f12fda7640908e79157144777fb15b80c227522a
Manoj V <manojvenki098@gmail.com>
2024-07-04 19:37:23 +0530

    Create  Implement Stack using Linked List.java
    https://www.geeksforgeeks.org/problems/implement-stack-using-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=implement-stack-using-linked-list

---
+++ DSA/[7] Stack/Implement Stack using Linked List.java
@@ -0,0 +1,32 @@
+
+class MyStack 
+{
+    class StackNode {
+        int data;
+        StackNode next;
+        StackNode(int a) {
+            data = a;
+            next = null;
+        }
+    }   
+    StackNode top=null;
+    
+    //Function to push an integer into the stack.
+    void push(int a) 
+    {
+        // Add your code here
+        StackNode newNode=new StackNode(a);
+        newNode.next=top;
+        top=newNode;
+    }
+    
+    //Function to remove an item from top of the stack.
+    int pop() 
+    {
+        // Add your code here
+        if(top==null) return -1;
+        int data=top.data;
+        top=top.next;
+        return data;
+    }
+}
