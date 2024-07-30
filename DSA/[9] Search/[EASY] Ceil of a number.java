9899f185a7df932cbd5447ff8b0f2b4a06666b3d
Manoj V <manojvenki098@gmail.com>
2024-07-30 17:41:12 +0530

    Create [EASY] Ceil of a number.java

---
+++ DSA/[9] Search/[EASY] Ceil of a number.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int arr[]={2,3,5,9,14,16,18};
+        System.out.println(bSearch(arr,15));
+    }
+    public static int bSearch(int []arr,int target)
+    {
+        int start=0;
+        int end=6;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target) return arr[mid];
+            else if(arr[mid]<target) start=mid+1;
+            else end=mid-1;
+        }
+        return arr[start];
+    }
+}
+
