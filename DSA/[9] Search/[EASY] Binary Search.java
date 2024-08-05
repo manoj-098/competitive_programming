64f32848fae5f9231bb1ed7ef40784e98241eec9
Manoj V <manojvenki098@gmail.com>
2024-08-05 18:48:29 +0530

    Create [EASY] Binary Search.java
    https://www.geeksforgeeks.org/problems/binary-search-1587115620/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card

---
+++ DSA/[9] Search/[EASY] Binary Search.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int binarysearch(int[] arr, int k) {
+        // Code Here
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]==k) return mid;
+            else if(arr[mid]>k) end=mid-1;
+            else start=mid+1;
+        }
+        return -1;
+    }
+}
