5032130eb2aec0bfd87c8eeca95c9aefefad7285
Manoj V <manojvenki098@gmail.com>
2024-08-06 10:25:17 +0530

    Update Cyclic Sort.java

--- DSA/[10] Sorting/Cyclic Sort.java
+++ DSA/[10] Sorting/Cyclic Sort.java
@@ -1,13 +1,27 @@
-int i = 0;
-while (i < arr.length) 
-{
-	int correctIndex = arr[i] - 1;
-	if (arr[i] != arr[correctIndex]) 
-	{
-		// Swap arr[i] with arr[correctIndex]
-		int temp = arr[i];
-		arr[i] = arr[correctIndex];
-		arr[correctIndex] = temp;
-	} 
-	else i++;
+class Solution {
+    public void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                // Swap arr[i] with arr[correctIndex]
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {3, 1, 5, 4, 2};
+
+        solution.cyclicSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
