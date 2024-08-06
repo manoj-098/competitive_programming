a422824efe608774830d8e33366e69870310747b
Manoj V <manojvenki098@gmail.com>
2024-08-06 16:05:13 +0530

    Create [EASY] Missing Number.java
    https://leetcode.com/problems/missing-number/

---
+++ DSA/[10] Sorting/[EASY] Missing Number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        //perform sort first
+        int idx=0;
+        while(idx<nums.length)
+        {
+            int correctIdx=nums[idx];//1
+            if(correctIdx!=nums.length && nums[idx]!=nums[correctIdx])//0!=
+            {
+                    int temp=nums[idx];
+                    nums[idx]=nums[correctIdx];
+                    nums[correctIdx]=temp;
+            }
+            else idx++;
+        }
+
+        //then find missing element
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=i) return i;
+        }
+        return nums.length;
+
+    }
+}
