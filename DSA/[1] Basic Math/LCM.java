3faf6bb51ec387b300f5f382a2ed2368122aaf4e
Manoj V <manojvenki098@gmail.com>
2024-08-08 21:45:41 +0530

    Update LCM.java

--- DSA/[1] Basic Math/LCM.java
+++ DSA/[1] Basic Math/LCM.java
@@ -33,3 +33,28 @@ class HelloWorld {
         return lcm(a, b, a);
     }
 }
+
+//LCM of Array of Numbers
+public class LCMOfArrayExample {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int lcmOfArray(int[] numbers) {
+        int lcm = numbers[0];
+        for (int i = 1; i < numbers.length; i++) 
+            lcm = (lcm * numbers[i]) / gcd(lcm, numbers[i]);
+        
+        return lcm;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = { 2, 7, 3, 9, 4 };
+        System.out.println("LCM of the array is: " + lcmOfArray(numbers));
+    }
+}
