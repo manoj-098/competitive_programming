3f7e929cc27a383578a5894fe3b7f0b727161ae3
Manoj V <manojvenki098@gmail.com>
2024-08-08 21:41:08 +0530

    Update Armstrong.java

--- DSA/[1] Basic Math/Armstrong.java
+++ DSA/[1] Basic Math/Armstrong.java
@@ -1,8 +1,19 @@
-if (b == 0)
-            return a;
- 
-        // call the gcd() method recursively by
-        // replacing a with b and b with
-        // modulus(a,b) as long as b != 0
-        else
-            return gcd(b, a % b);
+public class ArmstrongNumberCheck {
+    public static boolean isArmstrong(int num) {
+        int original = num, sum = 0;
+        int n = String.valueOf(num).length();
+        
+        while (num != 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, n);
+            num /= 10;
+        }
+        
+        return original == sum;
+    }
+
+    public static void main(String[] args) {
+        int num = 153;
+        System.out.println(num + " is Armstrong: " + isArmstrong(num));
+    }
+}
