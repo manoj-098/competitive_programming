9fae77d0a41242f516a98c6d2b4155e5981c8a1d
Manoj V <manojvenki098@gmail.com>
2024-08-09 11:31:11 +0530

    Create [EASY] Print N to 1 without loop.java

---
+++ DSA/[9] Recursion/[EASY] Print N to 1 without loop.java
@@ -0,0 +1,11 @@
+//GFG : https://www.geeksforgeeks.org/problems/print-n-to-1-without-loop/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=print-n-to-1-without-loop
+
+class Solution {
+
+    void printNos(int N) {
+        // code here
+        if(N==0) return;
+        System.out.print(N+" ");
+        printNos(--N);
+    }
+}
